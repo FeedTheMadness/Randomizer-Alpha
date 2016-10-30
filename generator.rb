ba0c838d9ceda1e770ae96d8398aa9f826050219
Adrien Augery <adrien.augery@gmail.com>
2016-10-30 14:32:16 +0100

    Ajout de commentaire

--- generator.rb
+++ generator.rb
@@ -1,9 +1,14 @@
 require "base62"
-
+#Initialisation du compteur
 n = 0
+
+#Démarage de la loop
 loop do
+  #Génération de la suite de caractères
   row = Array.new(Random.rand(6..14)){rand(62).base62_encode}.join
   puts row
+  #Initeration du compteur
   n += 1
+  #Arrête la boucle si le compteur atteint 100
   break if n == 100
 end
